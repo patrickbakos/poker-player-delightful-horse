37889ab06379b9a59cd9753f6090e6a67913b41f
Krisztian Juhasz <juhasz0511@gmail.com>
2019-05-27 13:45:56 +0200

    back to woriking version

--- player.py
+++ player.py
@@ -2,30 +2,29 @@ import json
 
 
 class Player:
-    VERSION = "1.7"
+    VERSION = "1.8"
 
     def betRequest(self, game_state):
-        bets = []
+        ##bets = []
 
-        for i in range(0, 5):
-            for bet in game_state['players'][i]['bet']:
-                bets.append(bet)
+        ##for i in range(0, 5):
+        ##    for bet in game_state['players'][i]['bet']:
+        ##        bets.append(bet)
 
 
 
         my_cards = []
         ##translated_state = json.load(game_state)
 
-        my_pot = 75
-        max_bet = max(bets)
+
 
         my_index = game_state["in_action"]
         for card in game_state["players"][my_index]["hole_cards"]:
             my_cards.append(card["rank"])
         if my_cards[0] == my_cards[1]:
-            return my_pot
+            return 75
         else:
-            return max_bet
+            return 10
 
     def showdown(self, game_state):
         pass
