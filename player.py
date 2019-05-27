d9038687fa6fa27bfa60478469c367d1eb8d1db5
Patrick <bakospatrick@gmail.com>
2019-05-27 14:02:29 +0200

    1.9

--- player.py
+++ player.py
@@ -26,7 +26,7 @@ class Player:
         for card in game_state["community_cards"]:
             community_cards_rank.append(card["rank"])
             community_cards_suit.append(card["suit"])
-        if my_cards_rank[0] == my_cards_rank[1] and my_cards_rank[0]:
+        if my_cards_rank[0] == my_cards_rank[1] or my_cards_rank[0] in community_cards_rank or my_cards_rank[1] in community_cards_rank:
             return 75
         elif my_cards_rank[0] == my_cards_rank[1] and my_cards_rank[0] in community_cards_rank:
             return 300
