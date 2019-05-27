d2924123bf3cb9c35c23e934f894bdd0c5b59116
Krisztian Juhasz <juhasz0511@gmail.com>
2019-05-27 11:27:35 +0200

    check the bets

--- player.py
+++ player.py
@@ -1,9 +1,21 @@
+import json
+
 
 class Player:
     VERSION = "1.1"
 
     def betRequest(self, game_state):
-        return 10
+        game_state = json.loads(game_state)
+        bets = []
+
+        for bet in game_state['players']['bet']:
+            bets.append(bet)
+        pot = 20
+
+        if max(bets) > pot:
+            return 0
+        else:
+            return pot
 
     def showdown(self, game_state):
         pass
