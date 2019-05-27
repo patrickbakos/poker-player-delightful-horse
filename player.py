49fed5a1030d2e27306757b0f24cae6344d2df31
Krisztian Juhasz <juhasz0511@gmail.com>
2019-05-27 12:11:48 +0200

    changed the pots

--- player.py
+++ player.py
@@ -2,7 +2,7 @@ import json
 
 
 class Player:
-    VERSION = "1.4.000"
+    VERSION = "1.5,"
 
     def betRequest(self, game_state):
         game_state = json.loads(game_state)
@@ -10,22 +10,22 @@ class Player:
 
         for bet in game_state['players']['bet']:
             bets.append(bet)
-        pot = 20
 
-        if max(bets) > pot:
-            return 0
-        else:
-            return pot
+
 
         my_cards = []
         ##translated_state = json.load(game_state)
+
+        my_pot = 75
+        max_bet = max(bets)
+
         my_index = game_state["in_action"]
         for card in game_state["players"][my_index]["hole_cards"]:
             my_cards.append(card["rank"])
         if my_cards[0] == my_cards[1]:
-            return 75
+            return my_pot
         else:
-            return 0
+            max_bet
 
     def showdown(self, game_state):
         pass
